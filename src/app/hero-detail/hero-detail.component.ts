28c2429c5c75c9f479adc23d8fa1b96aa2cf1e76
Larissa Bizzi <bizzi.larissa@gmail.com>
2023-11-30 15:31:43 -0500

    adding navigation to hero-detail

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HeroService } from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero!: Hero;
+
+  constructor(
+              private heroService: HeroService,
+              private location: Location,
+              private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,12 +9,10 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-    ) {}
+    private heroService: HeroService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -23,10 +21,5 @@ export class HeroesComponent implements OnInit {
       this.heroService.getHeroes().subscribe(
         (heroes) => (this.heroes = heroes)
         );
-  }
-
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
